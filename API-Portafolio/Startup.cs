4923b1af83cccfe53f3fde034a4ffc67bdcb4ed0
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-02-03 13:31:16 -0300

    implementando ruta post para subir archivos a buckets AWS

--- API-Portafolio/Startup.cs
+++ API-Portafolio/Startup.cs
@@ -1,36 +1,35 @@
 using ApplicationDb.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Amazon.S3;
+
+
 public class Startup
 {
     public Startup(IConfiguration configuration)
     {
         Configuration = configuration;
-            Console.WriteLine("Configurando");
-
+        Console.WriteLine("Configurando");
     }
 
     public IConfiguration Configuration { get; }
 
     public void ConfigureServices(IServiceCollection services)
     {
-
         services.AddControllers();
-        
+
         services.AddDbContext<ApplicationDbContext>(opt =>
         {
             var connectionString = Configuration.GetConnectionString("DefaultConnection");
             opt.UseNpgsql(connectionString).LogTo(Console.WriteLine, LogLevel.Information);
         });
 
-
-        
         services.AddSwaggerGen(c =>
         {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "Nombre de tu API", Version = "v1" });
         });
 
-        //CORS CONFIG
+        // CORS CONFIG
         services.AddCors(options =>
         {
             options.AddPolicy("AllowLocalhost3000",
@@ -42,23 +41,23 @@ public class Startup
                 });
         });
 
+        // AWS S3
+        services.AddAWSService<IAmazonS3>();
+
+
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-
         app.UseCors("AllowLocalhost3000");
 
         if (env.IsDevelopment())
         {
-            // ...
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Portafolio .NET"));
         }
 
-        // app.UseHttpsRedirection();
         app.UseAuthorization();
-
         app.UseRouting();
         app.UseEndpoints(endpoints =>
         {
