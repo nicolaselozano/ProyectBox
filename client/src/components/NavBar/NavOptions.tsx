58bb87dd25ca7ba8b620092e8bc5aa53ed1502b1
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-02-15 20:23:05 -0300

    fix nav 2

--- client/src/components/NavBar/NavOptions.tsx
+++ client/src/components/NavBar/NavOptions.tsx
@@ -1,10 +1,11 @@
 import Button_Nav from "../Button/Button_Nav";
+import style from "./NavOptions.module.css";
 
 const NavOptions = () => {
   return (
     <div>
-      <ul className="flex flex-col lg:flex-row lg:justify-around">
-        <li>
+      <ul className={`flex ${style.list__button}`}>
+        <li className={`${style.container__button}`}>
           <Button_Nav to="/">Inicio</Button_Nav>
         </li>
         <li>
