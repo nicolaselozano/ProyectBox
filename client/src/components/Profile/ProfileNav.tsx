90110d0642d8ebaf94fcedc5c0c32eaf10703804
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-06-14 17:15:44 -0300

    fix profile

--- client/src/components/Profile/ProfileNav.tsx
+++ client/src/components/Profile/ProfileNav.tsx
@@ -2,6 +2,7 @@
 import { UUID } from "crypto";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import Button_Nav from "../Button/Button_Nav";
 
 export interface User {
     email:string
@@ -27,7 +28,11 @@ const ProfileNav = () => {
 
     return (
         <div>
-            {user ? <h1>Bienvenido {user?.name}</h1> : null}
+            {user ? 
+                <div className="flex-col items-center justify-center ml-5">
+                    <h1>Bienvenido {user?.name}</h1> 
+                    <Button_Nav to="/pages/Profile">Profile</Button_Nav>
+                </div>: null}
         </div>
     )
 
